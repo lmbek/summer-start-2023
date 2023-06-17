c4d9be26c8787d22326244ebc489e6af723750ac
IdaDania <54858@student.eadania.dk>
2023-06-17 19:02:55 +0200

    IdaBox.cs : Added * class with method for setting Gizmo on box

---
+++ Assets/git-sync/Scripts/IdaBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.git_sync.Scripts
+{
+    public class IdaBox : MonoBehaviour
+    {
+        void OnDrawGizmosSelected()
+        {
+            // draw a red sphere at the transforms position 
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position, 3);
+        }
+    }
+}
